d9892aa5d9ed84b36415f612789ff75bdc6e8801
Shlok Kaushik <54725967+ShlokKaushik23@users.noreply.github.com>
2024-09-26 18:46:23 +0530

    Portfolio

---
+++ public/script/script.js
@@ -0,0 +1,378 @@
+const galleriesData = [
+    {
+        id: 1,
+        images: [
+
+            "./public/images/projects/metal/metaljewellery_1.webp",
+            "./public/images/projects/metal/metaljewellery_2.webp",
+            "./public/images/projects/metal/metaljewellery_3.webp",
+            "./public/images/projects/metal/metaljewellery_4.webp",
+            "./public/images/projects/metal/metaljewellery_5.webp",
+            "./public/images/projects/metal/metaljewellery_6.webp",
+            "./public/images/projects/metal/metaljewellery_7.webp",
+            "./public/images/projects/metal/metaljewellery_8.webp",
+            "./public/images/projects/metal/metaljewellery_9.webp",
+            "./public/images/projects/metal/metaljewellery_10.webp",
+            "./public/images/projects/metal/metaljewellery_11.webp",
+            "./public/images/projects/metal/metaljewellery_12.webp",
+
+        ]
+    },
+    {
+        id: 2,
+        images: [
+            "./public/images/projects/02_drapping/drape_8.webp",
+            "./public/images/projects/02_drapping/drape_7.webp",
+            "./public/images/projects/02_drapping/drape_9.webp",
+            "./public/images/projects/02_drapping/drape_11.webp",
+            "./public/images/projects/02_drapping/drape_12.webp",
+            "./public/images/projects/02_drapping/drape_10.webp",
+
+
+        ]
+    },
+    {
+        id: 3,
+        images: [
+            "./public/images/projects/02_drapping/drape_20.webp",
+            "./public/images/projects/02_drapping/drape_19.webp",
+            "./public/images/projects/02_drapping/drape_17.webp",
+            "./public/images/projects/02_drapping/drape_15.webp",
+            "./public/images/projects/02_drapping/drape_16.webp",
+            "./public/images/projects/02_drapping/drape_13.webp",
+        ]
+    },
+    {
+        id: 4,
+        images: [
+            "./public/images/projects/02_drapping/drape_32.webp",
+            "./public/images/projects/02_drapping/drape_30.webp",
+            "./public/images/projects/02_drapping/drape_26.webp",
+            "./public/images/projects/02_drapping/drape_27.webp",
+
+        ]
+    },
+    {
+        id: 5,
+        images: [
+            "./public/images/projects/02_drapping/drape_33.webp",
+            "./public/images/projects/02_drapping/drape_35.webp",
+            "./public/images/projects/02_drapping/drape_36.webp",
+            "./public/images/projects/02_drapping/drape_37.webp",
+            "./public/images/projects/02_drapping/drape_34.webp",
+        ]
+    },
+    {
+        id: 6,
+        images: [
+            "./public/images/projects/02_drapping/drape_4.webp",
+            "./public/images/projects/02_drapping/drape_3.webp",
+            "./public/images/projects/02_drapping/drape_2.webp",
+            "./public/images/projects/02_drapping/drape_1.webp",
+            "./public/images/projects/02_drapping/drape_6.webp",
+            "./public/images/projects/02_drapping/drape_5.webp",
+
+        ]
+    },
+    {
+        id: 7,
+        images: [
+            "./public/images/projects/02_drapping/drape_21.webp",
+            "./public/images/projects/02_drapping/drape_22.webp",
+            "./public/images/projects/02_drapping/drape_23.webp",
+            "./public/images/projects/02_drapping/drape_24.webp",
+
+        ]
+    },
+    {
+        id: 8,
+        images: [
+            "./public/images/projects/03_digital/digital_1.webp",
+            "./public/images/projects/03_digital/digital_2.webp",
+            "./public/images/projects/03_digital/digital_3.webp",
+            "./public/images/projects/03_digital/digital_4.webp",
+            "./public/images/projects/03_digital/digital_5.webp",
+            "./public/images/projects/03_digital/digital_6.webp",
+            "./public/images/projects/03_digital/digital_7.webp",
+            "./public/images/projects/03_digital/digital_8.webp",
+            "./public/images/projects/03_digital/digital_9.webp",
+            "./public/images/projects/03_digital/digital_10.webp",
+            "./public/images/projects/03_digital/digital_11.webp",
+            "./public/images/projects/03_digital/digital_12.webp",
+            "./public/images/projects/03_digital/digital_13.webp",
+        ]
+    },
+    {
+        id: 9,
+        images: [
+            "./public/images/projects/04_prints/prints_7.webp",
+            "./public/images/projects/04_prints/prints_6.webp",
+            "./public/images/projects/04_prints/prints_12.webp",
+            "./public/images/projects/04_prints/prints_2.webp",
+            "./public/images/projects/04_prints/prints_1.webp",
+            "./public/images/projects/04_prints/prints_3.webp",
+            "./public/images/projects/04_prints/prints_4.webp",
+            "./public/images/projects/04_prints/prints_5.webp",
+            "./public/images/projects/04_prints/prints_8.webp",
+            "./public/images/projects/04_prints/prints_9.webp",
+            "./public/images/projects/04_prints/prints_10.webp",
+            "./public/images/projects/04_prints/prints_11.webp",
+
+
+        ]
+    },
+    {
+        id: 10,
+        images: [
+            "./public/images/projects/05_mori/mori_1.webp",
+            "./public/images/projects/05_mori/mori_2.webp",
+            "./public/images/projects/05_mori/mori_3.webp",
+            "./public/images/projects/05_mori/mori_4.webp",
+            "./public/images/projects/05_mori/mori_5.webp",
+            "./public/images/projects/05_mori/mori_6.webp",
+            "./public/images/projects/05_mori/mori_7.webp",
+            "./public/images/projects/05_mori/mori_8.webp",
+            "./public/images/projects/05_mori/mori_9.webp",
+            "./public/images/projects/05_mori/mori_10.webp",
+            "./public/images/projects/05_mori/mori_11.webp",
+            "./public/images/projects/05_mori/mori_12.webp",
+            "./public/images/projects/05_mori/mori_13.webp",
+        ]
+    },
+    {
+        id: 11,
+        images: [
+            "./public/images/projects/06_wake/jorunalbook_1.webp",
+            "./public/images/projects/06_wake/jorunalbook_2.webp",
+            "./public/images/projects/06_wake/jorunalbook_3.webp",
+            "./public/images/projects/06_wake/jorunalbook_4.webp",
+            "./public/images/projects/06_wake/jorunalbook_5.webp",
+            "./public/images/projects/06_wake/jorunalbook_6.webp",
+            "./public/images/projects/06_wake/jorunalbook_7.webp",
+            "./public/images/projects/06_wake/jorunalbook_8.webp",
+            "./public/images/projects/06_wake/jorunalbook_9.webp",
+            "./public/images/projects/06_wake/jorunalbook_10.webp",
+
+        ]
+    },
+    {
+        id: 12,
+        images: [
+            "./public/images/projects/07_simple/sim_2.jpeg",
+            "./public/images/projects/07_simple/sim_1.jpeg",
+            "./public/images/projects/07_simple/sim_3.jpeg",
+        ]
+    },
+    {
+        id: 13,
+        images: [
+            "./public/images/projects/08_godness/Goddess_4.webp",
+            "./public/images/projects/08_godness/Goddess_5.webp",
+            "./public/images/projects/08_godness/Goddess_1.webp",
+            "./public/images/projects/08_godness/Goddess_2.webp",
+            "./public/images/projects/08_godness/Goddess_3.webp",
+
+        ]
+    },
+    {
+        id: 14,
+        images: [
+            "./public/images/projects/09_rewild/rewild_1.webp",
+            "./public/images/projects/09_rewild/rewild_2.webp",
+            "./public/images/projects/09_rewild/rewild_3.webp",
+            "./public/images/projects/09_rewild/rewild_4.webp",
+            "./public/images/projects/09_rewild/rewild_5.webp",
+            "./public/images/projects/09_rewild/rewild_6.webp",
+            "./public/images/projects/09_rewild/rewild_7.webp",
+            "./public/images/projects/09_rewild/rewild_8.webp",
+            "./public/images/projects/09_rewild/rewild_9.webp",
+            "./public/images/projects/09_rewild/rewild_10.webp",
+            "./public/images/projects/09_rewild/rewild_11.webp",
+            "./public/images/projects/09_rewild/rewild_12.webp",
+            "./public/images/projects/09_rewild/rewild_13.webp",
+            "./public/images/projects/09_rewild/rewild_14.webp",
+            "./public/images/projects/09_rewild/rewild_15.webp",
+        ]
+    }
+
+
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    galleriesData.forEach(galleryData => {
+        const section = document.getElementById(`gallery${galleryData.id}`);
+        section.innerHTML = `
+            <div class="indicators" id="indicators${galleryData.id}"></div>
+            <div class="image_section gallery">
+                ${galleryData.images.map((src, index) => `<a href="${src}" data-index="${index}"><img src="${src}" alt=""></a>`).join('')}
+            </div>
+        `;
+
+        // Ensure images load correctly
+        const imag = section.querySelectorAll('img');
+        imag.forEach(image => {
+            image.onload = () => {
+                image.style.display = 'block';  // Ensure proper rendering after load
+            };
+        });
+
+        lightGallery(section.querySelector('.image_section'), {
+            download: false,
+            fullscreen: false,
+            
+        });
+
+        const imageSection = section.querySelector('.image_section');
+        const indicators = section.querySelector(`#indicators${galleryData.id}`);
+        const images = imageSection.getElementsByTagName('img');
+        const numImages = images.length;
+        let currentIndex = 0;
+
+        function updateIndicators() {
+            indicators.innerHTML = '';
+            for (let j = 0; j < numImages; j++) {
+                const indicator = document.createElement('div');
+                if (j === currentIndex) {
+                    indicator.classList.add('indicate');
+                }
+                indicator.addEventListener('click', () => {
+                    currentIndex = j;
+                    scrollToImage();
+                });
+                indicators.appendChild(indicator);
+            }
+        }
+
+        function scrollToImage() {
+            const currentImage = images[currentIndex];
+            const offsetLeft = currentImage.parentElement.offsetLeft;
+            imageSection.scrollTo({
+                left: offsetLeft,
+                behavior: 'smooth'
+            });
+
+            updateIndicators();
+        }
+
+        function updateCurrentIndex() {
+            const sectionWidth = imageSection.clientWidth;
+            for (let i = 0; i < images.length; i++) {
+                if (images[i].offsetLeft < imageSection.scrollLeft + sectionWidth / 2) {
+                    currentIndex = i;
+                }
+            }
+            updateIndicators();
+        }
+
+        // Scroll event listener
+        imageSection.addEventListener('scroll', () => {
+            updateCurrentIndex();
+        });
+
+        window.addEventListener('resize', scrollToImage);
+
+        updateIndicators();
+        scrollToImage();
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var popupBtns = document.querySelectorAll('.popup-btn');
+    var popupWrap = document.querySelector('.popup-wrap');
+    var popupBox = document.querySelector('.popup-box');
+    var popupClose = document.querySelector('.popup-close');
+
+    popupBtns.forEach(function (btn) {
+        btn.addEventListener('click', function (e) {
+            popupWrap.style.display = 'block';
+            setTimeout(function () {
+                popupBox.classList.remove('transform-out');
+                popupBox.classList.add('transform-in');
+            }, 1); // Slight delay to ensure the display change is applied
+            e.preventDefault();
+        });
+    });
+
+    popupClose.addEventListener('click', function (e) {
+        popupWrap.style.display = 'none';
+        popupBox.classList.remove('transform-in');
+        popupBox.classList.add('transform-out');
+        e.preventDefault();
+    });
+});
+
+
+
+
+// Select all slider wrapper elements
+document.querySelectorAll('.slider-wrapper').forEach(function (sliderWrapper, index) {
+    // Create unique pagination and navigation selectors for each slider
+    const swiper = new Swiper(sliderWrapper, {
+        loop: true,
+        grabCursor: true,
+        autoplay: {
+            delay: 1000, // Adjust the delay time (3000ms = 3 seconds)
+            disableOnInteraction: false,
+        },
+        speed: 800,
+        spaceBetween: 30,
+        // Pagination bullets
+        pagination: {
+            el: sliderWrapper.querySelector('.swiper-pagination'),
+            clickable: true,
+            dynamicBullets: true
+        },
+        // Navigation arrows
+        navigation: {
+            nextEl: sliderWrapper.querySelector('.swiper-button-next'),
+            prevEl: sliderWrapper.querySelector('.swiper-button-prev'),
+        },
+        // Responsive breakpoints
+        breakpoints: {
+            0: {
+                slidesPerView: 1.5
+            },
+            768: {
+                slidesPerView: 2
+            },
+
+            1025: {
+                slidesPerView: 4
+            }
+        }
+    });
+});
+
+
+
+
+
+
+
+
+//  ========== MAKING THE CATEGORY SECTION STICKY AT THE TOP ON SCROLL ================ 
+// Get the category section element
+var categorySection = document.querySelector('.category_section');
+
+// Calculate the offset top of the category section
+var categorySectionOffsetTop = categorySection.offsetTop;
+
+// Function to handle scroll event
+function handleScroll() {
+    // Get the current scroll position
+    var scrollPosition = window.scrollY || window.pageYOffset;
+
+    // Check if the scroll position is greater than or equal to the category section's offset top
+    if (scrollPosition >= categorySectionOffsetTop) {
+        categorySection.classList.add('sticky'); // Add sticky class
+    } else {
+        categorySection.classList.remove('sticky'); // Remove sticky class
+    }
+}
+
+// Add scroll event listener to window
+window.addEventListener('scroll', handleScroll);
+
+// Initial call to handleScroll to check sticky state on page load
+handleScroll();
